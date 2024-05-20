58fc711b06c960f03c316645e3b66ca91daf0404
Jaime Conceição <jaimeconceicao@MacBook-Pro-de-Jaime.local>
2024-05-20 09:42:46 +0100

    character

--- character.js
+++ character.js
@@ -1,26 +1,30 @@
 function Character() {
-  this.y = height / 2;
-  this.x = width / 2;
-  this.push = 0.96;
+  this.y = 1.4*height/5;
+  this.x = width / 4;
+  this.push = 1.4;
   this.lift = -30;
   this.velocity = 0;
   this.diam = 80;
-  this.img = loadImage("space.png");
-  this.minHeight = 300;
+  this.img = loadImage("char escala.png");
+  this.minHeight = 1.85*height/5;
+  this.jumped = false;
+
 
   this.show = function () {
     // use this function to design the main character
     stroke(0);
     strokeWeight(2);
     fill(255);
-    image(this.img, this.x, this.y, this.diam, this.diam * 0.7368421053); // insert the raw png image propotion rate and use this.diam to input image width
+    image(this.img, this.x, this.y, this.diam, this.diam * 1.372222); // insert the raw png image propotion rate and use this.diam to input image width
   };
 
   this.goUp = function () {
-    this.velocity += this.lift;
-    console.log(this.velocity);
-  };
-
+    if (this.jumped == false){
+      this.velocity += this.lift;
+      console.log(this.velocity);
+      this.jumped = true;}
+    }
+    
   this.update = function () {
     this.velocity += this.push;
     this.velocity *= 0.9;
@@ -29,11 +33,14 @@ function Character() {
     if (this.y >= height - this.minHeight) {
       this.y = height - this.minHeight;
       this.velocity = 0;
+      if (this.jumped == true){
+        this.jumped = false;}
     }
 
     if (this.y < 0) {
       this.y = 0;
       this.velocity = 0;
     }
+   
   };
 }
